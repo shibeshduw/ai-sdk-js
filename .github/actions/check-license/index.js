d9aa2eaba290966ef5b0bd85d4e6632500646319
Shibesh Duwadi <shibesh.duwadi@sap.com>
2024-10-08 10:30:30 +0200

    stuff

--- .github/actions/check-license/index.js
+++ .github/actions/check-license/index.js
@@ -1,12 +1,3 @@
-import * as core from '@actions/core';
 import { checkLicenses } from '../../../scripts/check-licenses';
-
-async function run() {
-    try {
-      await checkLicenses();
-    } catch (error) {
-      if (error instanceof Error) core.setFailed(error.message);
-    }
-  }
-  
-  run();
+await checkLicenses();
+    
